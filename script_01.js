b6fe08549945c69959977d4db2da1c11c034f7e9
Odirax <dario.lucic@hotmail.de>
2023-05-10 10:21:12 +0200

    Vorbereitungswerte

--- script_01.js
+++ script_01.js
@@ -3,3 +3,26 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
+const ageJohn = 35;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage 
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+
+
+
+
+
+
